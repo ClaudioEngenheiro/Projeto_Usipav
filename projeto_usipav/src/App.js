dbe1261a55cc93567119eec67230a9282688f512
Eric <ikedayuji.2002@gmail.com>
2023-10-05 20:47:12 -0300

    Jornada de Aprendizagem – Otimização de Sistemas e IOT v1 05/10/2023

--- projeto_usipav/src/App.js
+++ projeto_usipav/src/App.js
@@ -3,9 +3,8 @@ import NavTelainicio from './navTelainicio';
 import NavMenu from './navMenu';
 import { Container, Row, Col } from 'react-bootstrap';
 import Cartoes from './Cartoes';
-import Constantes from './Constantes'
-//import { Footer } from 'react-bootstrap/lib/Modal';
 
+import backgroundImage from './imagens/planodefundo.png'; // Substitua pelo caminho correto da sua imagem de fundo
 
 function App() {
   return (
@@ -18,17 +17,16 @@ function App() {
         <Col xs={3} className="nebulous white bordamenuacesso mx-auto style"><h3>Menu Principal</h3>
           <NavMenu></NavMenu>
         </Col>          
-        <Col xs={9} className="nebulous white bordaconteudodireita"><h3>Usipav - Usinagem de Asfalto Ltda</h3>
-        <Cartoes></Cartoes>        
+        <Col xs={9} className="nebulous white bordaconteudodireita" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center 10%' }}>
+          <h3 style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)' }}>Usipav - Usinagem de Asfalto Ltda</h3>
+          <Cartoes></Cartoes>        
         </Col>
       </Row>
       <Row className="rodape">
-        <Col className="bg-warning bordatempo">Previsão de Tempo:
-        </Col>
-        <Col xs={5}className="bg-warning bordaDireito">Direitos Reservados: UseIA Engenharia de Software</Col>
+        <Col className="bg-warning bordatempo">Previsão de Tempo:</Col>
+        <Col xs={5} className="bg-warning bordaDireito">Direitos Reservados: UseIA Engenharia de Software</Col>
         <Col className="bg-warning bordausuario">Usuário:</Col>
-        <Col className="bg-warning bordadatahora">Data/Hora:
-        </Col>
+        <Col className="bg-warning bordadatahora">Data/Hora:</Col>
       </Row>
     </Container>
   );
