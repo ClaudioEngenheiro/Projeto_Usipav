bd400d15f21ae70cae7b5181a1ac6d2ec95c9a23
ikedayuji <93358246+ikedayuji@users.noreply.github.com>
2023-10-25 18:40:15 -0300

    Jornada v1

---
+++ Projeto_Jornada_de_Aprendizagem_Otimiza_o_de_Sistemas_e_IOT_2023/src/Graficos.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+const Grafico = () => {
+  const data = [
+    { name: 'Janeiro', Vendas: 65 },
+    { name: 'Fevereiro', Vendas: 59 },
+    { name: 'Março', Vendas: 80 },
+    { name: 'Abril', Vendas: 81 },
+    { name: 'Maio', Vendas: 56 },
+  ];
+
+  return (
+    <div>
+      <h2>Gráfico de Teste</h2>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="Vendas" stroke="rgba(75, 192, 192, 1)" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Grafico;
